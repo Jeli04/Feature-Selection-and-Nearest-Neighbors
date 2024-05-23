ba9e2cb9a338e085e6ca70e001852f2c49dcb28a
Jerry <jeli04.2020@gmail.com>
2024-05-22 22:28:00 -0700

    read data

--- src/validation.py
+++ src/validation.py
@@ -1,9 +1,20 @@
-class Validation:
-    def __init__(self, data, test_size=0.2) -> None:
-        pass
+import numpy as np
+
+class Validator:
+    def __init__(self, filename, test_size=0.2) -> None:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+        self.data = np.array([list(map(float, line.split())) for line in lines])
+        self.train = self.data[:int(len(self.data) * 0.8)]
+        self.test = self.data[int(len(self.data) * 0.8):]
 
     def k_fold(self, k):
         return 
 
     def eval(self):
         return
+
+
+test = Validator("data/large-test-dataset.txt")
+print(test.data)
