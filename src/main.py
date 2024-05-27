e7700f540d258cb5b0ece82a260d0f5de3caa72a
Vaneesha Singh <vsingh@Vaneeshas-MacBook-Pro.local>
2024-05-26 22:18:22 -0700

    add time to main and trace

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from searches import Problem
 from classifiers import Classifier
 
+import time
+
 def main():
     print("Welcome to the Feature Selection Algorithm.")
     
@@ -14,14 +16,25 @@ def main():
     
     problemObj = Problem(my_list)
     
+    start_time_feature = time.time()
+    
     if (algo == 1):
         bestSet = list(problemObj.greedy_forward_search())
     elif (algo == 2):
         bestSet = problemObj.greedy_backward_search()
-        
+    
+    end_time_feature = time.time()
+    elasped_feature = end_time_feature - start_time_feature
+    print("\nFeature Selection Elapsed Time:", elasped_feature, "seconds")
+    
     print("Using features ", bestSet)
+    start_time_classification = time.time()
     dataset= Classifier(filename="./data/small-test-dataset.txt")
     dataset.testTheData(dataset.test, bestSet)
+    
+    end_time_classification = time.time()
+    elapsed_classification = end_time_classification - start_time_classification
+    print("\nClassification Elapsed Time:", elapsed_classification, "seconds")
 
 if __name__ == '__main__':
     main()
