d0cbb9b928545f0aaf176526a68a330b31fd6c3a
Jerry <jeli04.2020@gmail.com>
2024-06-01 18:12:35 -0700

    added new traces:

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main():
     my_list = list(range(1, numFeatures+1))
     
     # initialize our objects
-    validator = Validator("data/small-test-dataset.txt")
+    validator = Validator("data/CS170_Spring_2024_Large_data__16.txt")
     problemObj = Problem(my_list)
     classifier = Classifier()
 
